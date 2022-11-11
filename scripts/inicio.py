20c7e04bab9f335741e3ed8bf1e16f5a2a6d4192
Ernesto Lomar <marioernestolomar@protonmail.ch>
2022-11-11 16:11:10 -0600

    Incorporando prueba de memoria eeprom.

--- scripts/inicio.py
+++ scripts/inicio.py
@@ -1,11 +1,52 @@
+##########################################
+# Autor: Ernesto Lomar
+# Fecha de creación: 09/11/2022
+# Ultima modificación: 11/11/2022
+#
+# Script principal del programa
+#
+##########################################
+
+# Librerías importadas
 from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import *
 import sys
+from eeprom_num_serie import cargar_num_serie
 
 class principal(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        # Cargar la interfaz gráfica
         uic.loadUi("../ui/inicio.ui", self)
+        
+        # Conectar los botones con sus funciones
+        self.label_img_reiniciar_prueba.clicked.connect(self.reiniciar_prueba)
+        self.label_reiniciar_prueba.clicked.connect(self.reiniciar_prueba)
+        self.label_img_reiniciar_raspberry.clicked.connect(self.reiniciar_raspberry)
+        self.label_reiniciar_raspberry.clicked.connect(self.reiniciar_raspberry)
+        self.label_img_apagar.clicked.connect(self.apagar_raspberry)
+        self.label_apagar.clicked.connect(self.apagar_raspberry)
+        
+        # Iniciamos pruebas
+        self.verificar_memoria_eeprom()
+        
+    def verificar_memoria_eeprom(self):
+        estado = cargar_num_serie()
+        if 'ERR' in str(estado['state_num_serie']) or 'NSxxxxx' in str(estado['state_num_serie']) and 'ERR' in str(estado['state_num_version']) or 'NVxxxxx' in str(estado['state_num_version']):
+            self.label_resultado_eeprom.setPixmap(QPixmap("../img/incorrecto.png"))
+        else:
+            self.label_resultado_eeprom.setPixmap(QPixmap("../img/comprobado.png"))
+        
+    def reiniciar_prueba(self):
+        print("Reiniciar prueba")
+        
+    def reiniciar_raspberry(self):
+        print("Reiniciar raspberry")
+        
+    def apagar_raspberry(self):
+        print("Apagar raspberry")        
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
