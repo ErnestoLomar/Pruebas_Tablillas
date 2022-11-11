44273f70b719818aceb9430e27ad4d5a444eca95
Ernesto Lomar <marioernestolomar@protonmail.ch>
2022-11-11 11:08:02 -0600

    Añadiendo eventos a labels

--- scripts/inicio.py
+++ scripts/inicio.py
@@ -12,8 +12,22 @@ import subprocess
 from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import sys
 from eeprom_num_serie import cargar_num_serie
+
+class RFIDWorker(QObject):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    finished = pyqtSignal()
+    progress = pyqtSignal(dict)
+    
+    def run(self):
+        estado = subprocess.run("nfc-poll", stdout=subprocess.PIPE, shell=True)
+        self.progress.emit({"estado": estado.stdout.decode()})
 
 class principal(QMainWindow):
     def __init__(self):
@@ -30,8 +44,11 @@ class principal(QMainWindow):
         self.label_img_apagar.mousePressEvent = self.apagar_raspberry
         self.label_apagar.mousePressEvent = self.apagar_raspberry
         
-        # Iniciamos pruebas
+        # Iniciamos prueba de memoria EEPROM
         self.verificar_memoria_eeprom()
+        
+        # Iniciamos prueba de RFID
+        self.runRFID()
         
     def verificar_memoria_eeprom(self):
         estado = cargar_num_serie()
@@ -42,9 +59,30 @@ class principal(QMainWindow):
             self.label_resultado_eeprom.setPixmap(QPixmap("../img/comprobado.png"))
             print("Memoria EEPROM correcta")
             
-    def verificar_rfid(self):
-        estado = subprocess.run("nfc-poll", stdout=subprocess.PIPE, shell=True)
-        print(estado.stdout.decode())
+    def runRFID(self):
+        try:
+            self.rfidThread = QThread()
+            self.rfidWorker = RFIDWorker()
+            self.rfidWorker.moveToThread(self.rfidThread)
+            self.rfidThread.started.connect(self.rfidWorker.run)
+            self.rfidWorker.finished.connect(self.rfidThread.quit)
+            self.rfidWorker.finished.connect(self.rfidWorker.deleteLater)
+            self.rfidThread.finished.connect(self.rfidThread.deleteLater)
+            self.rfidWorker.progress.connect(self.reportProgressRIFD)
+            self.rfidThread.start()
+        except Exception as e:
+            print("Error al iniciar el hilo de minicom: " + str(e))
+            
+    def reportProgressRIFD(self, res: dict):
+        try:
+            if 'UID' in res.decode():
+                self.label_resultado_eeprom.setPixmap(QPixmap("../img/comprobado.png"))
+                print("RFID correcto")
+            else:
+                self.label_resultado_eeprom.setPixmap(QPixmap("../img/incorrecto.png"))
+                print("RFID incorrecto")
+        except Exception as e:
+            print("inicio.py, linea 160: "+str(e))
         
     def reiniciar_prueba(self, event):
         print("Reiniciar prueba")
